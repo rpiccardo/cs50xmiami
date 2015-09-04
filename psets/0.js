d40662ee4561e15efeb8638a75a553a08d7e1f3f
rpiccardo <rebeca.piccardo@gmail.com>
2015-09-03 20:11:09 -0400

    comitted

--- psets/0.js
+++ psets/0.js
@@ -11,3 +11,14 @@ Why pay a fortune teller when you can just program your fortune yourself?
 */
 
 // write your solution here...
+
+
+var numChildren = 1;
+
+var partnerName = "Adrian Grenier";
+
+var geolocal = "Miami";
+
+var jobtitle = "reporter";
+
+console.log("You will be a " + jobtitle + "in Miami, and married to " + partnerName + "with " + numChildren + "kids." );
