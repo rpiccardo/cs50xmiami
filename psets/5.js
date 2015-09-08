1e8b95bf6c15ef41571d8121afd57589320a2dd8
rpiccardo <rebeca.piccardo@gmail.com>
2015-09-08 15:13:01 -0400

    updated 5.js

--- psets/5.js
+++ psets/5.js
@@ -10,6 +10,20 @@ For example:
   DrEvil(1000000): 1000000 dollars (pinky)
 */
 
+//prompt("enter an amount");
+
+//hard-coded amount to test function output in console
+var amount = 1000000;
+drEvil(amount);
+
+
 function drEvil(amount) {
   // write your solution here...
+  if(amount === 1000000)
+  {
+    console.log(amount + " dollars (pinky)")
+  }
+  else
+  console.log(amount + "dollars")
+
 }
